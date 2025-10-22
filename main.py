6e47e7fcd14a01fc91dd36053c628839c2805836
Ayar Maw <ayarlwinmaw@gmail.com>
2025-10-21 23:58:04 -0700

    added select box for data view

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ st.title('Weather Forecast')
 place = st.text_input('Enter your city name')
 days = st.slider('Forecast Days', 1, 5,
                  help='Select number of days between 1 and 5')
+option = st.selectbox("Select data to view",
+                      (
+                          "Temperature",
+                          "Sky",
+                          "Wind",
+                          "Precipitation",
+                      ))
+
+st.subheader(f"{option} for the next {days} days in {place}")
