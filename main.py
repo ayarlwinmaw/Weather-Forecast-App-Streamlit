e6d1a54c64bcc60b1588a9197355ad8bf23ca48a
Ayar Maw <ayarlwinmaw@gmail.com>
2025-10-21 23:55:30 -0700

    added title, input box for city and slider for numbers of days added

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 import streamlit as st
+
+st.title('Weather Forecast')
+place = st.text_input('Enter your city name')
+days = st.slider('Forecast Days', 1, 5,
+                 help='Select number of days between 1 and 5')
